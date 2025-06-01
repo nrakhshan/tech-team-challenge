a00efbd6f6c137dcd1b3a0df8f145c633b262b8f
ocfrog <171744752+ocfrog@users.noreply.github.com>
2025-05-31 23:55:53 -0400

    Add files via upload (yes)

--- src/App.js
+++ src/App.js
@@ -3,8 +3,16 @@ import './App.css';
 import data from './members_details.json'
 
 function App() {
+  let nerdsInfo = data.map(nerd => (
+    <div key={nerd.name}>
+      <h1>{nerd.name}</h1>
+      <p>{nerd.position.replaceAll('O', '🅾️')}</p>
+      <img src={nerd.picture}></img>
+    </div>
+  ));
   return (
     <div className="App">
+      {nerdsInfo}
     </div>
   );
 }
